be9a0eebad9b66e304268d20f3d80ad847d5f73f
Kaushal_Agrawal <kaushal.agrawal86@gmail.com>
2011-11-04 19:12:43 -0400

    Initial Codes

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 	include_once('conf/config.php');
 	include_once('fb/facebook_api.php');
@@ -19,4 +20,44 @@
 
 <body>	
 	
+<<<<<<< HEAD
 </body>
+=======
+	<?php 
+		if(isLoggedin()) echo ('Facebook login');
+		else echo $loginurl;
+	?>
+</body>
+=======
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	    <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
+	    <link rel="stylesheet" href="css/style.css"/>
+	    <script type="text/javascript">
+	    	var map;
+            var markers = new Array();
+	   		$(document).ready(function(){
+	   			var latlng = new google.maps.LatLng(27.70287,85.31824);
+			    var myOptions = {
+				    zoom: 2,
+				    center: latlng,
+	   			    mapTypeId: google.maps.MapTypeId.ROADMAP
+			    };
+			    map = new google.maps.Map(document.getElementById("my-map"),myOptions);
+			    GEvent.addListener(map, "click", function() {
+  					alert("You clicked the map.");
+				});
+	   		});
+	   		
+	   	</script>
+	</head>
+	<body>
+		<div id="page">
+			<div id="my-map" style="width:100%;height:300px;"></div>
+		</div>
+	</body>
+</html>
+>>>>>>> Initial Codes
+>>>>>>> Initial Codes
