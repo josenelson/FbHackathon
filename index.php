095221cfe33c263736750413f538a085f944beab
Jose Nelson Alves <jose.nelson.afonseca@gmail.com>
2011-11-04 18:56:34 -0400

    added php login

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
 <?php
-	require_once('conf/config.php');
+	include_once('conf/config.php');
+	include_once('fb/facebook_api.php');
+	session_start();
+	
+	$user = getCurrentUserInfo();
+	$logouturl = $_SESSION["logoutUrl"];
+	$loginurl = $_SESSION["loginUrl"];
+	
+	if(isLoggedin()) 
+		echo ('Facebook login');
+	else {
+		header('<a href="' . $loginurl.'">Login with Facebook</a>');
+		die();
+	}
+		
+
 ?>
 
 <body>
 	Let the hackathon begin!
+	
+	<?php 
+		if(isLoggedin()) echo ('Facebook login');
+		else echo $loginurl;
+	?>
 </body>
