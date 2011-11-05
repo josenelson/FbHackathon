b14375748c7788968c1d9ad7c6579745c60f7031
kartikeyadubey <kartikeyadubey@gmail.com>
2011-11-05 07:00:09 -0400

    delete an album based on event and albumid

---
+++ services/remove_album.php
@@ -0,0 +1,16 @@
+<?php
+
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+include_once('../conf/config.php');
+
+$eventid = isset($_GET["eventid"])? $_GET["eventid"]:"";
+$albumid = isset($_GET["albumid"])? $_GET["albumid"]:"";
+
+$query = "DELETE FORM images WHERE eventid='".$eventid."' and albumid='".$albumid."')";
+$result = mysql_query($query);
+echo $result;
+
+?>
