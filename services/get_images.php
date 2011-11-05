444bfe38413df8d482e575087def0e3395e2cc7d
kartikeyadubey <kartikeyadubey@gmail.com>
2011-11-05 10:08:11 -0400

    returning album pictures

--- services/get_images.php
+++ services/get_images.php
@@ -5,22 +5,33 @@ header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
 
 include_once('../conf/config.php');
+include_once('../fb/facebook_api.php');
 
 
-function get_images($eventid)
+
+$eventid = isset($_GET["eventid"]) ? $_GET["eventid"]:"";
+
+if(isset($_GET["userid"]))
 {
+	$query = "SELECT albumid FROM images WHERE eventid ='".$eventid."' and userid='".$_GET["userid"]."'";
+}
+else
+{
+	$query = "SELECT albumid FROM images WHERE eventid ='".$eventid."'";
+
+}
+
 	//Connection handle and get images based on eventid
-	$result = mysql_query("SELECT picid FROM images WHERE eventid ='".$eventid."'");
-
-	$eventData = array();
-
-	while($row = mysql_fetch_array($result))
+	$result = mysql_query($query);
+	$albumPictures = array();
+	$albumPictures["pictures"] = array();
+	while($row = mysql_fetch_assoc($result))
 	{
-		$temp = array();
-		$temp["picid"] = $row["picid"];
-		array_push($eventData, $temp);
+		foreach(getAlbumPictures($row["albumid"])->{"data"} as $pictureInAlbum)
+		{
+			array_push($albumPictures["pictures"], $pictureInAlbum->{"picture"});
+		}
 	}
 
 	//Return json data
-	return $eventData;
-}
+	echo json_encode($albumPictures);
