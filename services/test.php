8a6e4d90d6b0c8f9e59b28485a745bd9298b31cf
Jose Nelson Alves <jose.nelson.afonseca@gmail.com>
2011-11-05 03:15:53 -0400

    just making sure

--- services/test.php
+++ services/test.php
@@ -7,6 +7,33 @@
 
 	$user_name = $user_info->{"name"};
 	$user_id = $user_info->{"id"};
+   	
+?>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div id="fb-root"></div>
+	<script src="//connect.facebook.net/en_US/all.js"></script>
+	<script>
+  	FB.init({
+    	appId      : '<?php echo $_SESSION["appId"];?>', // App ID
+      	channelURL : '../fb/channel.html', // Channel File
+      	status     : true, // check login status
+      	cookie     : true, // enable cookies to allow the server to access the session
+      	oauth      : true, // enable OAuth 2.0
+      	xfbml      : true  // parse XFBML
 
-   	var_dump($user_info->{"name"});
+  	});
+  	
+  	FB.api('/me', function(response) {
+ 			 alert('Your name is ' + response.name);
+		});
+</script>
+</body>
+</html>
+      			
+ 
+ 
    
